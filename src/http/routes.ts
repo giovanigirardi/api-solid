ac63fe78b98fcda225cefc1953d02e3f65ca98b5
Giovani Girardi <giovanigg08@gmail.com>
2024-09-15 23:26:35 -0300

    feat: :sparkles: created jwt auth flow

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,10 +1,14 @@
-import type { FastifyInstance } from "fastify";
-
 import { authenticate } from "./controllers/authenticate";
+import { profile } from "./controllers/profile";
 import { register } from "./controllers/register";
+
+import type { FastifyInstance } from "fastify";
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post("/users", register);
 
 	app.post("/sessions", authenticate);
+
+	// Authenticated
+	app.get("/me", profile);
 }
