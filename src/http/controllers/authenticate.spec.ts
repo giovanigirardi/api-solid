fb367b3fbf0fa899fd336993edeb112726f80fc4
Giovani Girardi <giovanigg08@gmail.com>
2024-09-17 21:31:49 -0300

    test: :white_check_mark: created e2e test for register controller

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,6 +1,5 @@
 import { expect, test } from "vitest";
 
 test("ok", () => {
-	console.log("Test");
 	expect(1).toBe(1);
 });
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,6 +1,24 @@
-import { expect, test } from "vitest";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("ok", () => {
-	console.log("Test");
-	expect(1).toBe(1);
+import { app } from "@/app";
+
+describe("Register", () => {
+	beforeAll(async () => {
+		await app.ready();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	it("should be able to register a new user", async () => {
+		const response = await request(app.server).post("/users").send({
+			name: "John Doe",
+			email: "johndoe@email.com",
+			password: "123456",
+		});
+
+		expect(response.statusCode).toBe(201);
+	});
 });
