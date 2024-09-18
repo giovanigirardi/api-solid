6ce877ffc7ff2d91f1a2244389255a7e070864f3
Giovani Girardi <giovanigg08@gmail.com>
2024-09-17 21:23:25 -0300

    build: setup prisma test environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,40 @@
+import "dotenv/config";
+
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
+import { PrismaClient } from "@prisma/client";
+
 import type { Environment } from "vitest/environments";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseUrl(schema: string) {
+	if (!process.env.DATABASE_URL) {
+		throw new Error("DATABASE_URL is not set");
+	}
+
+	const url = new URL(process.env.DATABASE_URL);
+
+	url.searchParams.set("schema", schema);
+
+	return url.toString();
+}
 
 export default (<Environment>{
 	name: "prisma",
 	transformMode: "web",
 	async setup() {
-		console.log("Setting up Prisma environment");
+		const schema = randomUUID();
+		const databaseUrl = generateDatabaseUrl(schema);
+
+		process.env.DATABASE_URL = databaseUrl;
+
+		execSync("npx prisma migrate deploy");
 
 		return {
-			teardown() {
-				console.log("Tearing down Prisma environment");
+			async teardown() {
+				await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+				await prisma.$disconnect();
 			},
 		};
 	},
