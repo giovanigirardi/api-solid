bdaa13f2a1f0d5508d8c009ffa4b03234e797fff
Giovani Girardi <giovanigg08@gmail.com>
2024-09-17 21:34:40 -0300

    test: :white_check_mark: created e2e tests for authenticate controller

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,32 @@
-import { expect, test } from "vitest";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("ok", () => {
-	expect(1).toBe(1);
+import { app } from "@/app";
+
+describe("Authenticate", () => {
+	beforeAll(async () => {
+		await app.ready();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	it("should be able to authenticate", async () => {
+		await request(app.server).post("/users").send({
+			name: "John Doe",
+			email: "johndoe@email.com",
+			password: "123456",
+		});
+
+		const response = await request(app.server).post("/sessions").send({
+			email: "johndoe@email.com",
+			password: "123456",
+		});
+
+		expect(response.statusCode).toBe(200);
+		expect(response.body).toEqual({
+			token: expect.any(String),
+		});
+	});
 });
